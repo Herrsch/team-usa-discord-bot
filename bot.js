3a2f0144ddb0f04b75dd7511e614952d2d0b8010
Gene <gherrschaft@gmail.com>
2022-11-29 19:53:58 -0500

    Add cooldown for go live messages, add scoreboard updating

--- bot.js
+++ bot.js
@@ -44,26 +44,122 @@ const faceEmotes = [
 const gunUserId = "938529523811627038";
 const benUserId = "410621256140980225";
 
+const scoreboardMessageIds = [
+    "1047313677956681820",
+    "1047313679147864114",
+    "1047313680502628382",
+    "1047313681647673384",
+    "1047313682637537320"
+];
+
 client.on('ready', () => {
  console.log('Logged in!');
 });
 
-client.on('messageCreate', (msg) => {
-    if (!msg.content.charAt(0) == '~' || msg.mentions.members.size === 0) {
+async function getMovieCollection(channel) {
+    const message = await channel.messages.fetch(scoreboardMessageIds[0]);
+    var movieCollection = message.content.split("\n");
+
+    for (let i = 1; i < scoreboardMessageIds.length; i++) {
+        const message2 = await channel.messages.fetch(scoreboardMessageIds[i]);
+        if (!message2.content.startsWith("List Part ")) {
+            movieCollection = [...movieCollection, ...message2.content.split("\n")];
+        }
+    }
+
+    return movieCollection;
+}
+
+client.on('messageCreate', async (msg) => {
+    if (msg.content.charAt(0) != '~') {
         return;
     }
 
-    var timeoutDuration;
-    if (msg.content.substring(1, 6) == "shoot") {
-        timeoutDuration = 15 * 1000;
-    } else if (msg.content.substring(1, 5) == "kill") {
-        timeoutDuration = 60 * 1000;
-    } else {
-        return;
+    if (msg.mentions.members.size > 0) {
+        if (msg.content.substring(1, 6) == "shoot") {
+            shoot(msg, 15 * 1000);
+        } else if (msg.content.substring(1, 5) == "kill") {
+            shoot(msg, 60 * 1000);
+        }
     }
+    
+    if (msg.channel.id === "712134924815040522" ) { // Scoreboard channel
+        var collection = await getMovieCollection(msg.channel);
+        var separatorPos = msg.content.search(" "); // The first space after the bot command
+        if (separatorPos < 0) {
+            return;
+        }
+        const movieEntry = msg.content.substring(separatorPos + 1); // The sent message with the bot command excluded
+
+        if (msg.content.startsWith("~add")) {
+            separatorPos = movieEntry.search(" ");
+            if (separatorPos < 0) {
+                return;
+            }
+
+            const newMovieNumber = movieEntry.substring(0, separatorPos).match(/\d/g).join("");
+            const newMovieTitle = movieEntry.substring(separatorPos + 1);
+
+            collection.splice(newMovieNumber - 1, 0, newMovieNumber + ". " + newMovieTitle);
+            for (let i = newMovieNumber; i < collection.length; i++) {
+                separatorPos = collection[i].indexOf(".");
+                
+                collection[i] = (+i + +1) + collection[i].substring(separatorPos);
+            }
+
+
+        } else if (msg.content.startsWith("~remove")) {
+            const newMovieNumber = movieEntry.match(/\d/g).join("");
+            console.log(newMovieNumber);
+            collection.splice(newMovieNumber - 1, 1);
+            for (let i = newMovieNumber - 1; i < collection.length; i++) {
+                separatorPos = collection[i].indexOf(".");
+                
+                collection[i] = (+i + +1) + collection[i].substring(separatorPos);
+            }
+
+            
+        } else if (msg.content.startsWith("~update")) {
+            separatorPos = movieEntry.search(" ");
+            if (separatorPos < 0) {
+                return;
+            }
+
+            const newMovieNumber = movieEntry.substring(0, separatorPos).match(/\d/g).join("");
+            const newMovieTitle = movieEntry.substring(separatorPos + 1);
+
+            collection[newMovieNumber - 1] = newMovieNumber + ". " + newMovieTitle;
+
+        } else {
+            return;
+        }
+
+        updateScoreBoard(collection, msg.channel);
+        msg.delete();
+    }
+});
+
+client.on('voiceStateUpdate', async (oldState, newState) => {
+    if (newState.streaming && newState.channel != null && !oldState.streaming) {
+        const currentTimestamp = Date.now();
+        const generalChannel = await client.channels.cache.fetch("702142443608473602");
+        const messages = await generalChannel.messages.fetch({ limit: 10 }); // Check last 10 messages
+
+        for( let message of messages ) {
+            if (currentTimestamp - message.createdTimestamp > 60 * 60 * 1000) { // Check last hour
+                break;
+            }
+            if (message.member.id === gunUserId && message.content.startsWith(newState.member.displayName)) {
+                return;
+            }
+        }
+        generalChannel.send(newState.member.displayName + " has gone live!");
+    }
+});
+
+function shoot(msg, timeoutDuration) {
 
     var backfire = false;
-
     var shootMessage = faceEmotes[randomFaceIndex()];
     var tagMessage = "";
 
@@ -93,13 +189,35 @@ client.on('messageCreate', (msg) => {
     }
     msg.channel.send(tagMessage);
     msg.channel.send(shootMessage);
-});
+}
 
-client.on('voiceStateUpdate', (oldState, newState) => {
-    if (newState.streaming && newState.channel != null && !oldState.streaming) {
-        client.channels.cache.find(channel => channel.name === "general").send(newState.member.displayName + " has gone live!");
+async function updateScoreBoard(movieCollection, channel) {
+    var scoreboardMessageIndex = 0;
+    var scoreboardMessageContent = "";
+    var scoreboardMessageCharCount = 0;
+
+    for (let i = 0; i < movieCollection.length; i++) {
+        if (scoreboardMessageCharCount + movieCollection[i].length + 1 > 2000) {
+            await channel.messages.fetch(scoreboardMessageIds[scoreboardMessageIndex]).then( message => message.edit(content=scoreboardMessageContent));
+
+            scoreboardMessageIndex++;
+            scoreboardMessageContent = "";
+            scoreboardMessageCharCount = 0;
+            if (scoreboardMessageIndex >= scoreboardMessageIds.length) {
+                return;
+            }
+        }
+        if (scoreboardMessageCharCount > 0) {
+            scoreboardMessageContent += "\n"; // I THINK a newline only counts as one character
+            scoreboardMessageCharCount++;
+        }
+
+        scoreboardMessageContent += movieCollection[i];
+        scoreboardMessageCharCount += movieCollection[i].length;
     }
-});
+
+    channel.messages.fetch(scoreboardMessageIds[scoreboardMessageIndex]).then( message => message.edit(content=scoreboardMessageContent));
+}
 
 function randomFaceIndex() {
     min = Math.ceil(0);
