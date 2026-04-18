4921950d7f504d21bfd7c403e3d58d9087cf61cb
Lena <gherrschaft@gmail.com>
2026-04-18 16:10:02 -0400

    Adjust description

--- commands/gunKill.ts
+++ commands/gunKill.ts
@@ -3,7 +3,7 @@ import { SlashCommandBuilder, CommandInteraction } from 'discord.js';
 export default {
 	data: new SlashCommandBuilder()
 		.setName('kill')
-		.setDescription('Give user a long timeout. Costs ₿75.')
+		.setDescription('Give user a long timeout. Costs ₿40.')
         .addUserOption(option =>
             option.setName('target')
                 .setDescription('The user you\'re silencing.')
